e8f8e05a3b9859816440ff1e37a613be696373a8
Vladislav <vladislav.ponomarenko9930@gmail.com>
2023-08-28 12:19:50 +0300

    delete sessions

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-require('@babel/register');
 require('dotenv').config();
 
 const express = require('express');
--- config/serverConfig.js
+++ config/serverConfig.js
@@ -1,12 +1,7 @@
-const cookieParser = require('cookie-parser');
 const express = require('express');
 const path = require('path');
-const session = require('express-session');
-const sessionConfig = require('./sessionConfig');
 
 const config = (app) => {
-  app.use(cookieParser());
-  app.use(session(sessionConfig));
   app.use(express.urlencoded({ extended: true }));
   app.use(express.json());
   app.use(express.static(path.join(__dirname, '../public')));
